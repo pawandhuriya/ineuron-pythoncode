41625b953a4fabb8f5560a36a95566d3172effb7
pawandhuriya <pawandhuirya.outlook.com>
2023-02-25 13:03:41 +0530

    input_output python code

---
+++ python_input_output.py
@@ -0,0 +1,12 @@
+# Declare & assign variables in python
+int_var=10
+float_var=18.25
+string_var='you can yes you can'# another way "you can"\
+bool_var=True# firs case in capital
+
+
+# function in python  for output
+print("The value of int_var",int_var)
+print("The value of float_var",float_var)
+print("The value of string_var",string_var)
+print("The value of bool_var",bool_var)
