882befc38815a24a740f01ff9dc8d7f4d8a84f63
pawandhuriya <pawandhuirya.outlook.com>
2023-02-26 19:20:36 +0530

    loops

--- python_input_output.py
+++ python_input_output.py
@@ -10,3 +10,16 @@ print("The value of int_var",int_var)
 print("The value of float_var",float_var)
 print("The value of string_var",string_var)
 print("The value of bool_var",bool_var)
+
+
+#how to do thr type casting in python?
+int_var=int_var + 21
+casted_int_var=float(int_var)
+print("Int t float type casting for int_var",casted_int_var)
+
+
+#how to take input on python?
+name=input("enter the name:  ")
+age=input("Enter the age:  ")
+print("Your name is",name)
+print("Your age is",age)
