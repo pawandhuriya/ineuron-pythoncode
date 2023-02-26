5a61918513e16d6f9da1aca3044a5553a282a7ea
pawandhuriya <pawandhuirya.outlook.com>
2023-02-26 23:28:40 +0530

    new

---
+++ for_while_loop.py
@@ -0,0 +1,35 @@
+# for num in range(1,11,1):
+#     print(num)
+
+# excersize to calculate sum of given list elements using lop
+# int_list=[4,8,-2,10,5]
+
+
+# list_num=0
+# for num in int_list:
+#    list_num=list_num + num
+
+# print(list_num)
+
+#while loop syntax
+# num=9
+# counter=1
+# while counter<=10:
+#     ans=num*counter
+#     counter=counter+1
+#     print(ans)
+
+# while True:
+#     print("i neuron")
+#     break
+# how to break statement 
+int_list=[1,5,6,7,8,2,52,12]
+# Find the even value in given list
+num=0
+while num in int_list:
+    if num%2 ==0:
+        print("Even number value",num)
+        
+                  
+    
+     
---
+++ pthon_list.py
@@ -0,0 +1,44 @@
+#declare empty list append
+# L1=[]
+# L1.append(5)
+# L1.append(7)
+# L1.append(9)
+# print(L1)
+
+#create a list 1000
+# int_list=[]
+# for num in range(1,1001):
+#     int_list.append(num)
+#     print(num)
+# L1=[]
+# len_of_list=len(L1)
+# print("Length of lis",len_of_list)
+# list1=[1,"pawan",20,"Hi"]
+# list2=[1,"pawan",20,"Hi"]
+# print("List are equal ??",list1==list2)
+
+# list4=[1,2,3,4,5,6]
+# list5=[80,90,100,110]
+# result=list4 + list5
+# print(result)
+
+# list6=[10,15,20,25,30,35]
+# for num in list6:
+#     print(num)
+
+# print(list6[4])
+# Diffrence between append() and Extened()
+# list9=[20,3,40]
+# list10=["hi","ram","shaym"]
+# list9.append(list10)
+# print("list append",list9)
+# list9.extend(list10)
+# print("list extend",list9)
+list13=[20,30,40,50,60,80,90]
+print(list13[0: ])
+print(list13[3: ])
+print(list13[ : ])
+print(list13[2 : 6 ])
+print(list13[0: : 2])
+
+print(list13[-1])
