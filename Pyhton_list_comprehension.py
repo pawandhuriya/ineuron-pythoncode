d64c2cc468ca79e63f91f7c78c40637697e6c003
pawandhuriya <pawandhuirya.outlook.com>
2023-02-27 20:00:55 +0530

    compres list

---
+++ Pyhton_list_comprehension.py
@@ -0,0 +1,24 @@
+# for list comphereives
+# new list=[expersion (x) for n in old list if check]
+
+# for i in range(1,11):
+#     result.append(i)
+# print(result)
+
+#How to do it with the help of list comphernsion?
+
+# result=[x for x in range(1,11)]
+# print(result)
+
+# # Gte a list of all evene numbers between 1 to 50
+# list=[1,2,3,4,5,6,7,8]
+# result=[x for x in list if x%2==0 ]
+# print(result)
+
+# How to works with list
+list_a=[9,-1,2,-4,6,3,-4,0]
+print(result=[x for x in list_a if x>=0]+[x for x in list_a if x<=0])
+# print(result)
+
+
+#
